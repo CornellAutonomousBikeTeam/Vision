4f56074ccd0dc20d02fec8ed73a6b18746a24b07
RohitBandaru <rb696@cornell.edu>
2018-03-04 14:53:46 -0500

    mapping

--- Zed_API/RealTimePlotter.py
+++ Zed_API/RealTimePlotter.py
@@ -1,40 +1,19 @@
-
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import time
 
-import gmplot
-
-x = 0.0
-y = 0.0
-
-gmap = gmplot.GoogleMapPlotter(37.428, -122.145, 16)
+dx = 0.0
+dy = 0.0
+lat0 = 42.4440549
+lng0 = -76.482057
 
 while True:
-	x += random.random()
-	y += random.random()
-	time.sleep(0.5)
+	dx += 50*(np.random.random())
+	dy += 50*(np.random.random())
+	x = lat0 + (180/np.pi)*(dy/6378137)
+	y = lng0 + (180/np.pi)*(dx/6378137)/np.cos(lng0)
 
-	gmap.plot(latitudes, longitudes, 'cornflowerblue', edge_width=10)
-	gmap.scatter(more_lats, more_lngs, '#3B0B39', size=40, marker=False)
-	gmap.scatter(marker_lats, marker_lngs, 'k', marker=True)
-	gmap.heatmap(heat_lats, heat_lngs)
-
-	gmap.draw("mymap.html")
-
-plt.ion() ## Note this correction
-fig=plt.figure()
-plt.axis([0,1000,0,1])
-
-i=0
-x=list()
-y=list()
-
-while i <1000:
-    temp_y=np.random.random();
-    x.append(i);
-    y.append(temp_y);
-    plt.scatter(i,temp_y);
-    i+=1;
-    plt.show()
-    plt.pause(0.0001) #Note this correction
+	plt.scatter(x, y)
+	plt.draw()
+	plt.pause(0.00000000001)
