184da95fefc11b5b09eed3077a2466e4dea2e267
Raphael Thesmar <62407857+Raphael138@users.noreply.github.com>
2023-02-19 11:46:57 -0500

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -39,95 +39,107 @@ def getLabelsLISA():
     mkdir raph/images/train2017
     mkdir raph/labels
     mkdir raph/labels/train2017
+    mkdir raph/labels/val2017
 """
 
 # filter out classes out of coco we will not use
 def filterCOCO():
 
     coco_to_raph = {
-        person : 0
-        bicycle : 1
-        car : 2
-        motorcycle : 3
-        airplane: -1
-        bus : 4
-        train: -1
-        truck : 5
-        boat: -1
-        traffic light : 6
-        fire hydrant : 7
-        stop sign : 8
-        parking meter : 9
-        bench : 10
-        bird: -1
-        cat : 11
-        dog : 12
-        horse : 13
-        sheep: -1
-        cow: -1
-        elephant: -1
-        bear: -1
-        zebra: -1
-        giraffe: -1
-        backpack: -1
-        umbrella: -1
-        handbag: -1
-        tie: -1
-        suitcase: -1
-        frisbee: -1
-        skis: -1
-        snowboard: -1
-        sports ball: -1
-        kite: -1
-        baseball bat: -1
-        baseball glove: -1
-        skateboard: -1
-        surfboard: -1
-        tennis racket: -1
-        bottle: -1
-        wine glass: -1
-        cup: -1
-        fork: -1
-        knife: -1
-        spoon: -1
-        bowl: -1
-        banana: -1
-        apple: -1
-        sandwich: -1
-        orange: -1
-        broccoli: -1
-        carrot: -1
-        hot dog: -1
-        pizza: -1
-        donut: -1
-        cake: -1
-        chair: -1
-        couch: -1
-        potted plant: -1
-        bed: -1
-        dining table: -1
-        toilet: -1
-        tv: -1
-        laptop: -1
-        mouse: -1
-        remote: -1
-        keyboard: -1
-        cell phone: -1
-        microwave: -1
-        oven: -1
-        toaster: -1
-        sink: -1
-        refrigerator: -1
-        book: -1
-        clock: -1
-        vase: -1
-        scissors: -1
-        teddy bear: -1
-        hair drier: -1
-        toothbrush: -1
+        'person' : 0,
+        'bicycle' : 1,
+        'car' : 2,
+        'motorcycle' : 3,
+        'airplane': -1,
+        'bus' : 4,
+        'train': -1,
+        'truck' : 5,
+        'boat': -1,
+        'traffic light' : 6,
+        'fire hydrant' : 7,
+        'stop sign' : 8,
+        'parking meter' : 9,
+        'bench' : 10,
+        'bird': -1,
+        'cat' : 11,
+        'dog' : 12,
+        'horse' : 13,
+        'sheep': -1,
+        'cow': -1,
+        'elephant': -1,
+        'bear': -1,
+        'zebra': -1,
+        'giraffe': -1,
+        'backpack': -1,
+        'umbrella': -1,
+        'handbag': -1,
+        'tie': -1,
+        'suitcase': -1,
+        'frisbee': -1,
+        'skis': -1,
+        'snowboard': -1,
+        'sports ball': -1,
+        'kite': -1,
+        'baseball bat': -1,
+        'baseball glove': -1,
+        'skateboard': -1,
+        'surfboard': -1,
+        'tennis racket': -1,
+        'bottle': -1,
+        'wine glass': -1,
+        'cup': -1,
+        'fork': -1,
+        'knife': -1,
+        'spoon': -1,
+        'bowl': -1,
+        'banana': -1,
+        'apple': -1,
+        'sandwich': -1,
+        'orange': -1,
+        'broccoli': -1,
+        'carrot': -1,
+        'hot dog': -1,
+        'pizza': -1,
+        'donut': -1,
+        'cake': -1,
+        'chair': -1,
+        'couch': -1,
+        'potted plant': -1,
+        'bed': -1,
+        'dining table': -1,
+        'toilet': -1,
+        'tv': -1,
+        'laptop': -1,
+        'mouse': -1,
+        'remote': -1,
+        'keyboard': -1,
+        'cell phone': -1,
+        'microwave': -1,
+        'oven': -1,
+        'toaster': -1,
+        'sink': -1,
+        'refrigerator': -1,
+        'book': -1,
+        'clock': -1,
+        'vase': -1,
+        'scissors': -1,
+        'teddy bear': -1,
+        'hair drier': -1,
+        'toothbrush': -1
     }
 
-    ``
+    path = "/home/autobike/yolov7/coco/labels/"
+    for p in [path+"/"+"train2017", path+"/"+"val2017"]:
+        all_files = os.listdir(p)
+        for f in all_files:
+            annot_file = open(p+"/"+f)
+            annot_data = csv.reader(annot_file, delimiter = ' ')
+            for row in annot_data:
+                print(row)
+                print()
+            break
+        break
+
 
 
 
